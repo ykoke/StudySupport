13d4024eeb5f05e2bd76800686720ad0a1c37938
22fi051 <22fi051@ms.dendai.ac.jp>
2024-07-03 23:00:47 +0900

    form　insert

--- src/main/java/com/example/demo/controllers/TaskManagementController.java
+++ src/main/java/com/example/demo/controllers/TaskManagementController.java
@@ -2,11 +2,12 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.models.LessonCountdownModel;
 import com.example.demo.services.LessonCountdownService;
@@ -30,6 +31,22 @@ public class TaskManagementController {
       model.addAttribute("error", "An error occurred" + e.getMessage());
     }
     return "taskmanagement";
+  }
+
+  @GetMapping("/taskmanagement/form")
+  public String taskForm(Model model) {
+    model.addAttribute("lessonCountdownModel", new LessonCountdownModel());
+    return "taskForm";
+  }
+
+  @PostMapping("/taskmanagement/form/insert")
+  public String lessonInsert(LessonCountdownModel lessonCountdownModel, Model model) {
+    try {
+      lessonCountdownService.insert(lessonCountdownModel);
+    } catch (Exception e) {
+      model.addAttribute("error", "An error occurred" + e.getMessage());
+    }
+    return "redirect:/taskmanagement";
   }
 
   

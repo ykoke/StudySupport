52e7fd7b1aea99a653dc007796ebafd994ef4568
22fi051 <22fi051@ms.dendai.ac.jp>
2024-07-18 19:07:24 +0900

    users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-/* INSERT INTO users (username, password, role)
+INSERT INTO users (username, password, role)
 VALUES ('user', '$2a$10$a6c3I1GYtgYyENgNZ2O1mONQq/8WXUtaNx1pjzKpCSI4m01ymF9Ia', 'USER');
 INSERT INTO users (username, password, role)
 VALUES ('kouki', '$2a$10$4LY0z/URvnhc.9l4MnkS3./rO1K8Dh/QL7XcJ9MX0d4quXdYJKpCa', 'USER');
 INSERT INTO users (username, password, role)
-VALUES ('yuki', '$2a$10$P93DouLOAJEAi9yb/iMnrOIZZ5v2NN/mz6Y5nU.skag5LF/LK41kO', 'USER'); */
+VALUES ('yuki', '$2a$10$P93DouLOAJEAi9yb/iMnrOIZZ5v2NN/mz6Y5nU.skag5LF/LK41kO', 'USER');
 
 INSERT INTO countdown_dates (lesson, date,userid) VALUES ('ネットワークプログラミング', '2024-07-27',2);
 INSERT INTO countdown_dates (lesson, date,userid) VALUES ('情報技術基礎および演習', '2024-07-25',2);
